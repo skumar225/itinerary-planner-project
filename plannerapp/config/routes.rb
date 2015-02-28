741c5d3be6b19152e5e5561a4841b33255b34692
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-02-27 16:42:33 -0800

    added route definitions to routes file

--- plannerapp/config/routes.rb
+++ plannerapp/config/routes.rb
@@ -15,7 +15,40 @@ Rails.application.routes.draw do
     end
   end
 
-
+=begin
+Prefix Verb   URI Pattern                                            Controller#Action
+                     root GET    /                                                      sites#index
+             plan_reviews POST   /plans/:plan_id/reviews(.:format)                      reviews#create
+                          DELETE /plans/:plan_id/reviews/:id(.:format)                  reviews#destroy
+                    plans GET    /plans(.:format)                                       plans#index
+                          POST   /plans(.:format)                                       plans#create
+                 new_plan GET    /plans/new(.:format)                                   plans#new
+                edit_plan GET    /plans/:id/edit(.:format)                              plans#edit
+                     plan GET    /plans/:id(.:format)                                   plans#show
+                          PATCH  /plans/:id(.:format)                                   plans#update
+                          PUT    /plans/:id(.:format)                                   plans#update
+                          DELETE /plans/:id(.:format)                                   plans#destroy
+      destination_reviews POST   /destinations/:destination_id/reviews(.:format)        reviews#create
+                          DELETE /destinations/:destination_id/reviews/:id(.:format)    reviews#destroy
+        companion_reviews POST   /companions/:companion_id/reviews(.:format)            reviews#create
+                          DELETE /companions/:companion_id/reviews/:id(.:format)        reviews#destroy
+   destination_companions GET    /destinations/:destination_id/companions(.:format)     companions#index
+                          POST   /destinations/:destination_id/companions(.:format)     companions#create
+new_destination_companion GET    /destinations/:destination_id/companions/new(.:format) companions#new
+           edit_companion GET    /companions/:id/edit(.:format)                         companions#edit
+                companion GET    /companions/:id(.:format)                              companions#show
+                          PATCH  /companions/:id(.:format)                              companions#update
+                          PUT    /companions/:id(.:format)                              companions#update
+                          DELETE /companions/:id(.:format)                              companions#destroy
+             destinations GET    /destinations(.:format)                                destinations#index
+                          POST   /destinations(.:format)                                destinations#create
+          new_destination GET    /destinations/new(.:format)                            destinations#new
+         edit_destination GET    /destinations/:id/edit(.:format)                       destinations#edit
+              destination GET    /destinations/:id(.:format)                            destinations#show
+                          PATCH  /destinations/:id(.:format)                            destinations#update
+                          PUT    /destinations/:id(.:format)                            destinations#update
+                          DELETE /destinations/:id(.:format)                            destinations#destroy
+=end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
